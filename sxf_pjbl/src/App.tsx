5d6830fd37b4de2d8e165fc32756918b9ef77717
EduardoKrefta11 <eduardokrefta11@gmail.com>
2026-05-19 10:28:26 -0300

    Salvando arquivos antes do pull 19/05

---
+++ sxf_pjbl/src/App.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {  
+  const [message, setMessage] = useState(null)
+  const [id, setId] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const preventReload = async(e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({id, senha})
+    })
+
+    const data = await res.json()
+    setMessage(data.message)
+
+  }
+
+  return (
+    <>
+
+    <div className="containerMain">
+      
+      <h1>SXF</h1>
+
+      <p>{message}</p>
+
+      <div className="containerLogin">
+
+        <form className="formLogin" onSubmit={preventReload}>
+
+          <input 
+          type="text" 
+          placeholder="ID Registrado"
+          value={id}
+          onChange = {(e) => setId(e.target.value)}
+          />
+
+          <input 
+          type="password" 
+          placeholder="Senha"
+          value={senha}
+          onChange = {(e) => setSenha(e.target.value)}
+          />
+
+          <button type="submit">Logar</button>
+
+        </form> 
+
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
